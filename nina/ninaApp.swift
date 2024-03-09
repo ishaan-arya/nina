8b969d63f11c3c9e18e8b7d6fa3d30a7057b9fec
Ishaan Arya <iarya@umich.edu>
2024-03-09 17:04:35 -0500

    Initial Commit

---
+++ nina/ninaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ninaApp.swift
+//  nina
+//
+//  Created by Ishaan Arya on 09/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ninaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
