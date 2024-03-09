231c7be5fe58bbe458f66f2f24747b50e7fb64b5
jeetsudhir <asudhir@umich.edu>
2024-03-09 17:45:33 -0500

    Added keyword extraction and similarity scores

--- nina/Utils/Extract.swift
+++ nina/Utils/Extract.swift
@@ -1,6 +1,8 @@
 import Foundation
 import AppKit
 import PDFKit
+import NaturalLanguage
+
 
 class Extract {
     func extractTextFromFolder(_ folderURL: URL, completion: @escaping ([String]) -> Void) {
@@ -34,5 +36,35 @@ class Extract {
         }
         
         completion(textContent)
+    }
+    func extractKeywords(from fileURL: URL) -> Set<String> {
+        let text = try! String(contentsOf: fileURL, encoding: .utf8)
+        let tagger = NLTagger(tagSchemes: [.lexicalClass])
+        tagger.string = text
+
+        var keywords: Set<String> = []
+        var currentWord = ""
+
+        tagger.enumerateTags(in: text.startIndex..<text.endIndex, unit: .word, scheme: .lexicalClass, options: []) { tag, tokenRange in
+            if let tag = tag, (tag.rawValue == "Noun" && tag.rawValue != "Determiner") || tag.rawValue == "Adjective" || tag.rawValue == "ProperNoun" {
+                currentWord = String(text[tokenRange]).lowercased()
+                keywords.insert(currentWord)
+            }
+            return true
+        }
+
+        return keywords
+    }
+
+    func jaccardSimilarity(setA: Set<String>, setB: Set<String>) -> Double {
+        let intersection = setA.intersection(setB)
+        let union = setA.union(setB)
+        return Double(intersection.count) / Double(union.count)
+    }
+
+    func determineFileSimilarity(fileURLA: URL, fileURLB: URL) -> Double {
+        let keywordsA = extractKeywords(from: fileURLA)
+        let keywordsB = extractKeywords(from: fileURLB)
+        return jaccardSimilarity(setA: keywordsA, setB: keywordsB)
     }
 }
